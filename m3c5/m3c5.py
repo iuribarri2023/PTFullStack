d5e0ece2a184383691b2f99a63dd26d13b0cb76e
iuribarri2023 <151474777+iuribarri2023@users.noreply.github.com>
2024-05-22 00:17:51 +0200

    Create m3c5.py

---
+++ m3c5/m3c5.py
@@ -0,0 +1,21 @@
+#Creación de un bucle for
+lista_numeros=[1,2,3,4,5]
+for num in lista_numeros:
+    print(num)
+#Función que suma 3 números
+def sumar(a,b,c):
+    suma= a+b+c
+    print(suma)
+sumar(1,2,3)
+#Función lambda que suma 3 números
+sumar_tres_numeros= lambda a,b,c,:a+b+c
+resultado = sumar_tres_numeros(1,2,3)
+print(resultado)
+#Comprobación de nombre en la lista
+nombre= "Enrique"
+lista_nombre= ["Jessica","Paul","George","Henry","Adán"]
+for item in lista_nombre:
+    if nombre == item:
+        print("El nombre está en la lista!") 
+    else:
+        print ("El nombre no está en la lista")
