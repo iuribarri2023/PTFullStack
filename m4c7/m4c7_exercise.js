972c33f32118e6e1f2e1b7b0e0c8e83ab5e8f5f5
iuribarri2023 <151474777+iuribarri2023@users.noreply.github.com>
2024-06-18 23:35:37 +0200

    Create m4c7_exercise.js

---
+++ m4c7/m4c7_exercise.js
@@ -0,0 +1,7 @@
+const numManipulation =  function (a,b,c,d) {
+    const mult= (a+b)*(c+d)
+    if(mult>50){
+        console.log("¡El número es mayor que 50!",mult)
+    }
+    else console.log("¡El número es menor que 50!",mult)
+}
